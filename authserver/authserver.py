f031b4b85a5b5cf620a3ba138384601f8eb40e8a
Miguel Gonzalez <migonzalvar@gmail.com>
2013-09-20 19:20:46 +0200

    Add database connection and html templates

--- authserver/authserver.py
+++ authserver/authserver.py
@@ -1,19 +1,102 @@
+import hashlib
 import json
+import os.path
+import sqlite3
 
-from flask import Flask, request
+from flask import Flask, request, render_template, g
+
+
+OLPC_XS_DB = '/home/idmgr/identity.db'
+DATABASE = os.path.abspath(os.path.join(os.path.dirname(__file__), 'test.db'))
+
+
+def connect_to_database(database):
+    return sqlite3.connect(database)
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database(DATABASE)
+    return db
+
+
+def get_olpc_xs_db():
+    db = getattr(g, '_olpc_xs_db', None)
+    if db is None:
+        db = g._olpc_xs_db = connect_to_database(OLPC_XS_DB)
+    return db
+
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+
+class Idmgr:
+    """Laptops registered on idmagr.
+
+    Correspond to a register in table ``laptop`` on idmgr database. The
+    schema is defined as:
+
+    .. code:: sql
+
+        CREATE TABLE laptops (
+            serial VARCHAR(20) NOT NULL,
+            nickname VARCHAR(200) NOT NULL,
+            full_name VARCHAR(100) NOT NULL,
+            pubkey TEXT NOT NULL,
+            uuid VARCHAR(100),
+            lastmodified TEXT DEFAULT '1970-11-12 12:34:56',
+            class_group INTEGER,
+            PRIMARY KEY (serial)
+        );
+    """
+
+    def pkey_hash(self):
+        return hashlib.sha1(self.pubkey).hexdigest()
+
+    def get_by_pkey_hash(self):
+        return
+
+
+class User(object):
+    @classmethod
+    def get_by_pkey_hash(self, pkey_hash):
+        query = "SELECT username FROM users WHERE pkey_hash = ?"
+        args = (pkey_hash, )
+        cur = get_db().execute(query, args)
+        rv = cur.fetchall()
+        cur.close()
+        return rv[0] if rv else None
+
 
 app = Flask(__name__)
 app.debug = True
 
+
+@app.teardown_appcontext
+def close_connection(exception):
+    for db_name in ('_database', '_olpc_xs_db'):
+        db = getattr(g, db_name, None)
+        if db is not None:
+            db.close()
+
+
 @app.route('/')
 def index():
-    pkey_hash = None
+    pkey_hash, user = None, None
     cookie = request.cookies.get('xoid')
     if cookie:
         xoid = json.loads(cookie)
         if xoid:
             pkey_hash = xoid.get('pkey_hash')
-    return "OK! <pre>{}</pre>".format(pkey_hash)
+    if pkey_hash:
+        user = User.get_by_pkey_hash(pkey_hash)
+    return render_template("index.html", name=pkey_hash, user=user)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
