cc03c931baecd68245f2685a4d5cfdc814083a4b
Miguel Gonzalez <migonzalvar@gmail.com>
2013-09-26 22:10:43 +0200

    Apply GPLv3+ license

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# Copyright © 2013 Miguel González <migonzalvar@activitycentral.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+
 from setuptools import setup
 
 
@@ -21,4 +36,14 @@ setup(
     author=u'Miguel González',
     author_email='migonzalvar@activitycentral.com',
     url='https://github.com/migonzalvar/authserver',
+    license = "GNU GPLv3+",
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Flask',
+        'Intended Audience :: Education',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+    ],
 )
