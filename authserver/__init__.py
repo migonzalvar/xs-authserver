b23810d25f7f5f6211ecef524746a248cb3bd78f
Miguel Gonzalez <migonzalvar@gmail.com>
2013-09-26 14:52:26 +0200

    Create initialization database function

--- authserver/__init__.py
+++ authserver/__init__.py
@@ -23,6 +23,14 @@ def get_db():
     return db
 
 
+def init_db():
+    with app.app_context():
+        db = get_db()
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+        db.commit()
+
+
 def get_olpc_xs_db():
     db = getattr(g, '_olpc_xs_db', None)
     if db is None:
